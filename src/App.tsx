44a81b9fcaa1dcb4c982d3ea1636aaae0810f90c
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-05-30 21:06:12 +0200

    Add assets and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <p>Hello</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/:countryCode" element={<p>Details</p>} />
+        <Route path="*" element={<p>Error</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
