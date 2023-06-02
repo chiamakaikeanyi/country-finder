b5258e130fd6acfe03fbde2bf4528abcf4541349
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-06-02 14:04:47 +0200

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Details from "./pages/Details/Details";
-import styles from "./App.module.scss";
+import "./App.scss";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Header />
-        </header>
-        <main className={styles.main}>
+      <div className="container">
+        <Header />
+        <main className="main">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/:countryCode" element={<Details />} />
