88eef7ae0eecd7ecff52a0a7d62172812452264e
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-06-03 10:57:20 +0200

    Add styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
+import Button from "../Button/Button";
 import { MoonIcon } from "../Icons";
 
 const Header: React.FC = () => {
@@ -11,10 +12,12 @@ const Header: React.FC = () => {
           <h1>Where in the world?</h1>
         </Link>
       </nav>
-      <div className={styles.theme_switch__wrapper}>
-        <MoonIcon />
-        <span className={styles.theme_switch}>Dark mode</span>
-      </div>
+
+      <Button
+        label="Dark mode"
+        icon={<MoonIcon />}
+        customClass={styles.theme_switch}
+      />
     </header>
   );
 };
