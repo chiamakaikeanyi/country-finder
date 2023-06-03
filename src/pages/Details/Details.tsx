c1d96b74d2cdcb6a33092a31a72d484d56808c83
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2023-06-03 11:21:49 +0200

    Add details page

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,127 +1,220 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Helmet } from "react-helmet";
-
-import { getCountryByCode } from "../../api-client";
+import { getCountries, getCountryByCode } from "../../api-client";
 import styles from "./Details.module.scss";
+import Button from "../../components/Button/Button";
+import EmptyState from "../../components/EmptyState/EmptyState";
+import { ArrowLeftIcon } from "../../components/Icons";
+import { ICountry } from "../../types/Country";
 
 export default function Details() {
   const { countryCode } = useParams();
-  const [country, setCountry] = useState<Record<string, any> | null>(null);
+  const [country, setCountry] = useState<ICountry | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     if (countryCode) {
+      setIsLoading(true);
       getCountryByCode(countryCode).then((data) => {
-        if (data) setCountry(data[0]);
+        if (data && data.status !== 400) {
+          setCountry(data[0]);
+        } else {
+          setError(true);
+        }
+        setIsLoading(false);
       });
+    }
+  }, [countryCode]);
+
+  /**
+   * Temporal solution
+   * TODO: Use Context
+   */
+  const [countries, setCountries] = useState<ICountry[] | null>(null);
+  const [borders, setBorders] = useState<ICountry[] | null>(null);
+
+  useEffect(() => {
+    getCountries().then((data) => {
+      setCountries(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const getBorderCountries = (countryCodes: string[]) =>
+      countries?.filter((country) => countryCodes.includes(country.cca3));
+
+    if (country?.borders) {
+      const borderCountries = getBorderCountries(country.borders);
+
+      if (borderCountries) setBorders(borderCountries);
     }
   }, [countryCode]);
 
   return (
     <section data-testid="details_container" className={styles.container}>
-      <Helmet>
-        <title>{`${country?.name?.common} - Country Finder`}</title>
-        <meta
-          name="description"
-          content={`${country?.name?.common} is a country located in ${country?.continents?.[0]} and is made up of ${country?.population} people`}
-        />
-        ‍
-      </Helmet>
-      <article>
-        <img
-          src={country?.flags.svg}
-          loading="lazy"
-          alt={
-            country?.flags?.alt
-              ? country?.flags?.alt
-              : `${country?.name.common} flag`
-          }
-          width="150px"
-          className={styles.image}
-        />
+      {country?.name?.common && (
+        <Helmet>
+          <title>{`${country?.name?.common} - Country Finder`}</title>
+          <meta
+            name="description"
+            content={`${country?.name?.common} is a country located in ${country?.region} and is made up of ${country?.population} people`}
+          />
+        </Helmet>
+      )}
 
-        <div className={styles.body}>
-          <h2 className={styles.title}>{country?.name.common}</h2>
-          {country?.name?.nativeName && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Native Name(s):</span>
-              {Object.values(country.name.nativeName).map((nativeName: any) => (
-                <span className={styles.info__content} key={nativeName.common}>
-                  {nativeName.common}
-                </span>
-              ))}
-            </p>
-          )}
-          <p className={styles.info}>
-            <span className={styles.info__title}>Population:</span>
-            <span className={styles.info__content}>{country?.population}</span>
-          </p>
-          <p className={styles.info}>
-            <span className={styles.info__title}>Region:</span>
-            <span className={styles.info__content}>{country?.region}</span>
-          </p>
-          {country?.subregion && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Sub Region:</span>
-              <span className={styles.info__content}>{country.subregion}</span>
-            </p>
-          )}
-          {country?.capital && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Capital:</span>
-              <span className={styles.info__content}>{country.capital}</span>
-            </p>
-          )}
-        </div>
+      <Button icon={<ArrowLeftIcon />} label="Back" />
 
-        <div className={styles.body}>
-          <p className={styles.info}>
-            <span className={styles.info__title}>Top Level Domain:</span>
-            <span className={styles.info__content}>
-              {country?.tld.join(", ")}
-            </span>
-          </p>
-          {country?.currencies && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Currencies:</span>
-              {Object.values(country.currencies).map((currency: any) => (
-                <span className={styles.info__title} key={currency.name}>
-                  {currency.name}
-                </span>
-              ))}
-            </p>
-          )}
-          {country?.languages && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Languages:</span>
-              <span className={styles.info__content}>
-                {Object.values(country.languages).join(", ")}
-              </span>
-            </p>
-          )}
-          {country?.car && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Driver&apos;s Side:</span>
-              {Object.values(country.car.side)}
-            </p>
-          )}
-          {country?.timezones && (
-            <p className={styles.info}>
-              <span className={styles.info__title}>Timezones:</span>
-              {country.timezones}
-            </p>
-          )}
-        </div>
+      <article className={styles.wrapper}>
+        {isLoading || error ? (
+          <EmptyState
+            message={
+              isLoading
+                ? "Loading..."
+                : "Country not found. Please try another one."
+            }
+          />
+        ) : (
+          <>
+            <img
+              src={country?.flags.svg}
+              loading="lazy"
+              alt={
+                country?.flags?.alt
+                  ? country?.flags?.alt
+                  : `${country?.name.common} flag`
+              }
+              width="150px"
+              className={styles.image}
+            />
 
-        {country?.borders && (
-          <div className={styles.body}>
-            <span className={styles.info__title}>Border Countries:</span>
-            {country.borders.map((country: string) => (
-              <Link to={`/${country.toLowerCase()}`} key={country}>
-                {country}
-              </Link>
-            ))}
-          </div>
+            <div>
+              <h2 className={styles.heading}>{country?.name.common}</h2>
+              <div className={styles.content}>
+                <div>
+                  {country?.name?.nativeName && country?.languages && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>
+                        Native Name(s):
+                      </span>
+                      <span className={styles.info__content}>
+                        {
+                          country.name.nativeName[
+                            Object.keys(country.languages)[0]
+                          ]?.common
+                        }
+                      </span>
+                    </p>
+                  )}
+                  <p className={styles.info}>
+                    <span className={styles.info__title}>Population:</span>
+                    <span className={styles.info__content}>
+                      {country?.population?.toLocaleString()}
+                    </span>
+                  </p>
+                  <p className={styles.info}>
+                    <span className={styles.info__title}>Region:</span>
+                    <span className={styles.info__content}>
+                      {country?.region}
+                    </span>
+                  </p>
+                  {country?.subregion && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>Sub Region:</span>
+                      <span className={styles.info__content}>
+                        {country.subregion}
+                      </span>
+                    </p>
+                  )}
+                  {country?.capital && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>Capital:</span>
+                      <span className={styles.info__content}>
+                        {country.capital}
+                      </span>
+                    </p>
+                  )}
+                </div>
+
+                <div>
+                  <p className={styles.info}>
+                    <span className={styles.info__title}>
+                      Top Level Domain:
+                    </span>
+                    <span>{country?.tld.join(", ")}</span>
+                  </p>
+                  {country?.currencies && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>Currencies:</span>
+                      {Object.values(country.currencies).map(
+                        (currency: any) => (
+                          <span
+                            className={styles.info__content}
+                            key={currency.name}
+                          >
+                            {currency.name}
+                          </span>
+                        )
+                      )}
+                    </p>
+                  )}
+                  {country?.languages && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>Languages:</span>
+                      <span className={styles.info__content}>
+                        {Object.values(country.languages).join(", ")}
+                      </span>
+                    </p>
+                  )}
+                  {country?.car && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>
+                        Driver&apos;s Side:
+                      </span>
+                      <span className={styles.info__content}>
+                        {Object.values(country.car.side)}
+                      </span>
+                    </p>
+                  )}
+                  {country?.timezones && (
+                    <p className={styles.info}>
+                      <span className={styles.info__title}>Timezone(s):</span>
+                      <span className={styles.info__content}>
+                        {country.timezones.join(", ")}
+                      </span>
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <div className={styles.more_content}>
+                <>
+                  <span className={styles.info__title}>Border Countries:</span>
+                  <ul className={styles.more_content__info}>
+                    {borders ? (
+                      borders.map((border) => (
+                        <li
+                          key={border.name.common}
+                          className={styles.more_content__item}
+                        >
+                          <Link to={`/${border?.cca3.toLowerCase()}`}>
+                            <Button
+                              label={border.name.common}
+                              customClass={styles.border_btn}
+                            />
+                          </Link>
+                        </li>
+                      ))
+                    ) : (
+                      <span className={styles.more_content__item}>None</span>
+                    )}
+                  </ul>
+                </>
+              </div>
+            </div>
+          </>
         )}
       </article>
     </section>
