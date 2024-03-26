6a4e9e76da26efdb1f69305884e41d07ee66a2ff
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2024-03-26 21:55:44 +0100

    Add cluster group

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Icon } from "leaflet";
 import PropTypes from "prop-types";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 import { formatNumber } from "../../utils";
 
@@ -22,15 +23,21 @@ const Map = (props: { filteredCountries: ICountry[] | undefined }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {props.filteredCountries?.map((country: ICountry) => (
-        <Marker position={country?.latlng} icon={markerIcon} key={country.cca3}>
-          <Popup>
-            <b>{country?.name?.common}</b>
-            <br />
-            <b>Population</b>: {formatNumber(country?.population)}
-          </Popup>
-        </Marker>
-      ))}
+      <MarkerClusterGroup chunkedLoading>
+        {props.filteredCountries?.map((country: ICountry) => (
+          <Marker
+            position={country?.latlng}
+            icon={markerIcon}
+            key={country.cca3}
+          >
+            <Popup>
+              <b>{country?.name?.common}</b>
+              <br />
+              <b>Population</b>: {formatNumber(country?.population)}
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
     </MapContainer>
   );
 };
